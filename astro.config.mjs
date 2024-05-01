18743ebbd693d392f8885e46bafc81d207b5ee4f
Don Freer Group <don@donfreer.dev>
2024-04-30 20:12:01 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,15 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://www.kognitivemindshift.com',
-  build: 'directory','file',
-  integrations: [tailwind()]
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(), tailwind()],
+    site: `http://www.kognitivemindshift.com`,
+    output: 'static',
+    outDir: './docs',
+    build: {
+      assets: 'astro'
+    }
 });
