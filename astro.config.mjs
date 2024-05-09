72e78787bfc95c1ec2600ecb5de35c203c6dcc31
Don Freer Group <don@donfreer.dev>
2024-05-08 22:41:42 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-    site: `https://www.kognitivemindshift.com`
+    site: `https://www.kognitivemindshift.com`,
     }
 });
